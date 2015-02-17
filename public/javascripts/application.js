25a65a05f0f5bc56a49a826be4def62e03a39317
saulosantiago <saulodasilvasantiago@gmail.com>
2015-02-17 00:14:58 -0200

    starting _o_ \o_ \o/ /o/ \o\ \o/

---
+++ public/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery/dist/jquery
+//= require underscore/underscore
+//= require backbone/backbone
+//= require backbone.babysitter/lib/backbone.babysitter
+//= require backbone.wreqr/lib/backbone.wreqr
+//= require marionette/lib/backbone.marionette
