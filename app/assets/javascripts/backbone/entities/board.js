1a72bd7ddf91d2cd3eec645d87c76bf36ab9be37
saulosantiago <saulodasilvasantiago@gmail.com>
2015-03-07 20:06:39 -0300

    change boot rule

--- app/assets/javascripts/backbone/entities/board.js
+++ app/assets/javascripts/backbone/entities/board.js
@@ -9,19 +9,44 @@ this.VelhaMania.module('Entities', function(Entities, App, Backbone, Marionette,
     })
 
     Entities.Board = Backbone.Collection.extend({
+        model: Entities.Position,
+        bestMoves: [
+            ['a1', 'a2', 'a3'],
+            ['b1', 'b2', 'b3'],
+            ['c1', 'c2', 'c3'],
+            ['a1', 'b2', 'c3'],
+            ['c1', 'b2', 'a3'],
+            ['a1', 'b1', 'c1'],
+            ['a2', 'b2', 'c2'],
+            ['a3', 'b3', 'c3'],
+        ],
+
+        initialize: function() {
+            this.add([
+                { name: 'a1' }, { name: 'a2' }, { name: 'a3' },
+                { name: 'b1' }, { name: 'b2' }, { name: 'b3' },
+                { name: 'c1' }, { name: 'c2' }, { name: 'c3' }
+            ]);
+        }
     })
 
     API = {
-        init: function() {
-            console.log('-----------')
+        getBoard: function() {
             if (board == null) {
                 board = new Entities.Board();
-                board.add([
-                    { name: 'a1' }, { name: 'a2' }, { name: 'a3' },
-                    { name: 'b1' }, { name: 'b2' }, { name: 'b3' },
-                    { name: 'c1' }, { name: 'c2' }, { name: 'c3' }
-                ]);
             }
+
+            return board;
+        },
+
+        boardInit: function() {
+            return API.getBoard();
+        },
+
+        setOptions: function(options) {
+            board
+                .findWhere({ name: options.position })
+                .set({ ownerPosition: options.ownerPosition });
 
             window.board = board;
             return board;
@@ -29,10 +54,14 @@ this.VelhaMania.module('Entities', function(Entities, App, Backbone, Marionette,
     };
 
     App.reqres.setHandler('board:entities', function() {
-        return API.init();
+        return API.boardInit();
     });
 
     App.reqres.setHandler('get:positions:board:entities', function() {
-        return API.init();
+        return API.getBoard();
+    });
+
+    App.reqres.setHandler('set:position:board:entities', function(options) {
+        return API.setOptions(options);
     });
 });
--- app/assets/javascripts/backbone/entities/boot.js
+++ app/assets/javascripts/backbone/entities/boot.js
@@ -2,76 +2,28 @@ this.VelhaMania.module('Entities', function(Entities, App, Backbone, Marionette,
     var API;
 
     Entities.Boot = Backbone.Model.extend({
-        defaults: {
-            positions: [
-                'a1', 'a2', 'a3',
-                'b1', 'b2', 'b3',
-                'c1', 'c2', 'c3'
-            ],
+        played: function(options) {
+            board = App.request('get:positions:board:entities');
 
-            bestMoves: [
-                ['a1', 'a2', 'a3'],
-                ['b1', 'b2', 'b3'],
-                ['c1', 'c2', 'c3'],
-                ['a1', 'b2', 'c3'],
-                ['c1', 'b2', 'a3'],
-                ['a1', 'b1', 'c1'],
-                ['a2', 'b2', 'c2'],
-                ['a3', 'b3', 'c3'],
-            ]
-        },
+            opponentPositions = board.where({ ownerPosition: options.opponent })
+            myPositions = board.where({ ownerPosition: options.me })
+            freePositions = board.where({ ownerPosition: _.noop })
 
-        parse: function(positions) {
-            var map = {}
-            console.log(this.attributes);
-            _.each(this.get('positions'), function(i, position){
-                console.log(position);
-                map[position] = 'free';
-            });
-
-            if (positions) {
-                _.each(positions.opponent, function(i, position){
-                    map[position] = 'opponent';
-                });
-
-                _.each(positions.me, function(i, position){
-                    map[position] = 'me';
-                });
-            }
-
-            console.log(map)
-            return { map: map };
-        },
-
-        played: function() {
-            return
-            freePositions = _.difference(this.positions, positions.opponent);
-
-
-            realCombinations = _.extends(freePositions, positions.me);
-            combinationTotal = realCombinations.length
-
-            indices = _.map(bestMoves, function(combinations, i) {
-                differences = _(realCombinations).difference(combinations);
-
-                if (combinationTotal - combinationTotal === 3) {
-                    return i;
-                }
+            _.each(board.bestMoves, function(i, positions) {
+                _.difference(opponentPositions, positions)
+                _.difference(opponentPositions, myPositions)
             })
-
-
         }
     })
 
     API = {
-        played: function(positions) {
-            model = new Entities.Boot(positions, { parse: true })
-            window.foo = model;
-            return model.played()
+        played: function(options) {
+            model = new Entities.Boot()
+            return model.played(options);
         }
     };
 
-    App.reqres.setHandler('played:boot:entity', function(positions) {
-        return API.played(positions)
+    App.reqres.setHandler('played:boot:entity', function(options) {
+        return API.played(options)
     });
 });
