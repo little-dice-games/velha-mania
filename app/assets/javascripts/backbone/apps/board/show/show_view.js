94238ee6da65afa176dd387d9f4cd8037e39d887
saulosantiago <saulodasilvasantiago@gmail.com>
2015-05-08 03:00:40 -0300

    Layout for turn into game board

--- app/assets/javascripts/backbone/apps/board/show/show_view.js
+++ app/assets/javascripts/backbone/apps/board/show/show_view.js
@@ -10,7 +10,33 @@ this.VelhaMania.module('BoardApp.Show', function (Show, App, Backbone, Marionett
 
     Show.TurnView = Marionette.ItemView.extend({
         template: 'board/show/templates/turn',
-        className: 'turn'
+        className: function () {
+            var className = 'turn';
+
+            if (this.model && this.model.get('itsMe')) {
+                className += ' is-my-turn';
+            }
+
+            return className;
+        },
+
+        templateHelpers: function (target) {
+            var message = function () {
+                var text;
+
+                if (target.itsMe) {
+                    text = 'Sua Vez';
+                } else {
+                    text = 'Vez do ' + target.username;
+                }
+
+                return text;
+            };
+
+            return {
+                message: message()
+            };
+        }
     });
 
     Show.PositionView = Marionette.ItemView.extend({
