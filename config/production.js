6e433ef84f3a33ef36476da289149b4f1e54bea5
Leonardo Tegon <ltegon93@gmail.com>
2016-05-20 11:23:47 -0300

    Try again

--- config/production.js
+++ config/production.js
@@ -1,13 +1,13 @@
 var config = {
     assetsPath: [
+        'public/assets',
         'app/assets/images',
         'app/assets/stylesheets',
         'app/assets/javascripts',
         'app/assets/font',
         'vendor/assets/javascripts/bower_components'
     ],
-    viewsPath: 'views',
-    watchDir: '/app/assets/'
+    viewsPath: 'views'
 };
 
 module.exports = config;
